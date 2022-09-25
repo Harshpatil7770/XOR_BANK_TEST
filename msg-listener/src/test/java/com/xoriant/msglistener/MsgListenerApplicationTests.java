700955a4fa1f254a13fb9268c193904d62c6e36a
harsh <harshawardhanpatil9343@gmail.com>
2022-09-25 12:33:52 +0530

    Message Listener Service added

---
+++ msg-listener/src/test/java/com/xoriant/msglistener/MsgListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoriant.msglistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
