164a69c9e588fa34a9829eb0c773c7b41e8ed100
harsh <harshawardhanpatil9343@gmail.com>
2022-12-26 02:32:49 +0530

    Config-server name printed in logs

--- Admin-Service/src/main/java/com/xoriant/bank/model/ErrorCode.java
+++ Admin-Service/src/main/java/com/xoriant/bank/model/ErrorCode.java
@@ -3,13 +3,13 @@ package com.xoriant.bank.model;
 public enum ErrorCode {
 
 	NEW_BRANCH_ADDITION_FAILED("ErrorCode - Failed While Adding new Branch ", 1000),
-	EXISTING_BRANCH_UPDATION_FAILED("ErrorCode - Failed while updating the existing branch ",1001),
-	PUBLISHING_MSG_TO_QUEUE_FAILED("ErrorCode - Failed while publishing message to queue ",1002),
-	DELETION_OF_BRANCH_FAILED("Error code - Failed while deleting existing branch",1003),
-	NEW_MANAGER_ADDITION_FAILED("ErrorCode - Failed while adding new Manager details",1004),
-	FAILED_FETCHING_MANAGER_DETAILS("ErrorCode - Failed while fetching data from database",1005),
-	FAILED_UPDATING_THE_DETAILS("ErrorCode - Failed while updating the details",1006),
-	FAILED_FETHCING_BRANCH_DETAILS("ErrorCode - Failed while fetching the details",1007);
+	EXISTING_BRANCH_UPDATION_FAILED("ErrorCode - Failed while updating the existing branch ", 1001),
+	PUBLISHING_MSG_TO_QUEUE_FAILED("ErrorCode - Failed while publishing message to queue ", 1002),
+	DELETION_OF_BRANCH_FAILED("Error code - Failed while deleting existing branch", 1003),
+	NEW_MANAGER_ADDITION_FAILED("ErrorCode - Failed while adding new Manager details", 1004),
+	FAILED_FETCHING_MANAGER_DETAILS("ErrorCode - Failed while fetching data from database", 1005),
+	FAILED_UPDATING_THE_DETAILS("ErrorCode - Failed while updating the details", 1006),
+	FAILED_FETHCING_BRANCH_DETAILS("ErrorCode - Failed while fetching branch details", 1007);
 
 	private String description;
 
